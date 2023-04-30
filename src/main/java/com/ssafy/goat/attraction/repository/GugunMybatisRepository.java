d4c9af79e4e1f7210c7d5189fd9496926ae7b6e2
qhfhdls <79788971+DongHyun22@users.noreply.github.com>
2023-04-30 15:21:03 +0900

    feat: attractionRepository 작성 완료

--- src/main/java/com/ssafy/goat/attraction/repository/GugunMybatisRepository.java
+++ src/main/java/com/ssafy/goat/attraction/repository/GugunMybatisRepository.java
@@ -1,4 +1,18 @@
 package com.ssafy.goat.attraction.repository;
 
-public class GugunMybatisRepository {
+import com.ssafy.goat.attraction.Gugun;
+import com.ssafy.goat.attraction.mapper.AttractionMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class GugunMybatisRepository implements GugunRepository{
+    private final AttractionMapper attractionMapper;
+    @Override
+    public List<Gugun> findBySidoCode(int sidoCode) {
+        return attractionMapper.findBySidoCode(sidoCode);
+    }
 }
--- src/main/java/com/ssafy/goat/attraction/repository/SidoMybatisRepository.java
+++ src/main/java/com/ssafy/goat/attraction/repository/SidoMybatisRepository.java
@@ -1,4 +1,18 @@
 package com.ssafy.goat.attraction.repository;
 
-public class SidoMybatisRepository {
+import com.ssafy.goat.attraction.Sido;
+import com.ssafy.goat.attraction.mapper.AttractionMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class SidoMybatisRepository implements SidoRepository{
+    private final AttractionMapper attractionMapper;
+    @Override
+    public List<Sido> findAll() {
+        return attractionMapper.findAll();
+    }
 }
