fb9d72934efc559d733104bc01e184623ca76a0c
leeyr0412 <leeyr0412@naver.com>
2023-05-01 11:09:57 +0900

    refactor: 어노테이션 추가

--- src/main/java/com/ssafy/goat/hotplace/repository/HotPlaceQueryMybatisRepository.java
+++ src/main/java/com/ssafy/goat/hotplace/repository/HotPlaceQueryMybatisRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.goat.hotplace.repository;
 import com.ssafy.goat.hotplace.dto.HotPlaceDetailDto;
 import com.ssafy.goat.hotplace.dto.HotPlaceListDto;
 import com.ssafy.goat.hotplace.dto.HotPlaceSearch;
+import com.ssafy.goat.hotplace.mapper.HotplaceMapper;
 import com.ssafy.goat.util.DBConnectionUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -20,31 +21,34 @@ import java.util.Optional;
 @Repository
 @RequiredArgsConstructor
 public class HotPlaceQueryMybatisRepository implements HotPlaceQueryRepository {
-    private final HotPlaceQueryRepository hotPlaceQueryRepository;
+
+    private final HotplaceMapper mapper;
+
+//    private final HotPlaceQueryRepository hotPlaceQueryRepository;
 
     @Override
     public Optional<HotPlaceDetailDto> findDetailById(Long hotPlaceId) {
-        return hotPlaceQueryRepository.findDetailById(hotPlaceId);
+        return mapper.findDetailById(hotPlaceId);
     }
 
     @Override
     public List<HotPlaceListDto> findByCondition(HotPlaceSearch condition) {
-        return hotPlaceQueryRepository.findByCondition(condition);
+        return mapper.findByCondition(condition);
     }
 
     @Override
     public List<HotPlaceListDto> findByMemberId(Long memberId) {
-        return hotPlaceQueryRepository.findByMemberId(memberId);
+        return mapper.findByMemberId(memberId);
     }
 
     @Override
     public List<HotPlaceListDto> findFavoritesByMemberId(Long memberId, int pageNum, int amount) {
-        return hotPlaceQueryRepository.findFavoritesByMemberId(memberId, pageNum, amount);
+        return mapper.findFavoritesByMemberId(memberId, pageNum, amount);
     }
 
     @Override
     public int doFavorite(Long memberId, Long hotPlaceId) {
-        return hotPlaceQueryRepository.doFavorite(memberId, hotPlaceId);
+        return mapper.doFavorite(memberId, hotPlaceId);
 
     }
 
--- src/main/java/com/ssafy/goat/trend/mapper/TrendMapper.java
+++ src/main/java/com/ssafy/goat/trend/mapper/TrendMapper.java
@@ -2,9 +2,11 @@ package com.ssafy.goat.trend.mapper;
 
 import com.ssafy.goat.trend.Trend;
 import com.ssafy.goat.trend.dto.TrendViewDto;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.Optional;
 
+@Mapper
 public interface TrendMapper {
 
     int save(int contentId);
